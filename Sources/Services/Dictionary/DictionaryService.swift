4eae9b32a1f6c5de4cd7181395715f242a9879a6
SamFlamingo <cdsaad@gmail.com>
2026-02-02 11:19:27 -0500

    Complete Wiktionary dictionary lookups

--- Sources/Services/Dictionary/DictionaryService.swift
+++ Sources/Services/Dictionary/DictionaryService.swift
@@ -4,6 +4,13 @@ protocol DictionaryService {
     func lookup(word: String, language: Language) async throws -> DictionaryEntry?
 }
 
+enum DictionaryLookupError: Error {
+    case invalidURL
+    case networkFailure
+    case decodingFailure
+    case notFound
+}
+
 struct DictionaryEntry {
     let lemma: String
     let partOfSpeech: String?
